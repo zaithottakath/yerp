f056059dd83bb0671ada23538934832fea9c27c4
zaithottakath <zaithottakath@gmail.com>
2013-06-17 17:46:01 -0700

    Created app, changed database.yml to use pg, and installed rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yerp::Application.config.secret_token = '63dbd47114b61985e84565537e2d81a608ab190c6995dc4c337ae72681e8d4ddc00fe65ed7f23d822dfbdbf6136472f7f0fc38ab9fa4542bbb12c79d93c3c82f'
